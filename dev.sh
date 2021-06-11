e60ca4c92418a59c184999c3d9c8c97b91ae62ae
xirxo <xirxo.official@hotmail.com>
2021-06-11 13:44:43 +0800

    PM2 config file

--- dev.sh
+++ dev.sh
@@ -1,13 +1,19 @@
+clear
+
 if test -d ./build ; then
-    rm -rf ./build/*
+    rm -rf ./build/
 fi
 
-npm run build:watch &> /dev/null &
+npm run build:watch &
 sleep 10
 
+clear
+
 if command -v nodemon &> /dev/null ; then
+    clear
     nodemon -q ./build/index.js &
 else
+    clear
     node ./build/index.js &
 fi
 
